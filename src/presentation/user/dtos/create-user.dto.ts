925f8ddb24f8ebb995a51e6866cd7880c1056b99
William Koller <williamkoller404@gmail.com>
2022-10-26 19:01:29 -0300

    feat: :art: add create and update user dto

---
+++ src/presentation/user/dtos/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDTO {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
---
+++ src/presentation/user/dtos/update-user.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateUserDTO {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
