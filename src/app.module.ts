5b2f54423fbb61daa420fb347b9fb9f10b271e00
William Koller <williamkoller404@gmail.com>
2022-10-26 19:02:57 -0300

    feat: :art: add user repository and typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './infra/ioc/health.module';
+import { EnvironmentConfigModule } from './infra/config/environment-config/environment-config.module';
+import { TypeOrmConfigModule } from './infra/db/typeorm/typeorm.module';
+import { LoggerModule } from './infra/logger/logger.module';
+import { ExceptionsModule } from './infra/exceptions/exceptions.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    EnvironmentConfigModule,
+    TypeOrmConfigModule,
+    LoggerModule,
+    ExceptionsModule,
+  ],
   controllers: [],
   providers: [],
 })
